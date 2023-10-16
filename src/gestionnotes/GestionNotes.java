632e9e5495bb7b6754a9e6f42425575793341860
TOM JAMMES <tom.jammes@iut.local>
2023-10-16 15:28:59 +0200

    Premier commit

---
+++ src/gestionnotes/GestionNotes.java
@@ -0,0 +1,50 @@
+/*
+ * GestionNotes.java                                            16 oct. 2023
+ * IUT de Rodez, info2, pas de droits d'auteur
+ */
+package gestionnotes;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Application de gestion de notes d'un étudiant 
+ * @author tom.jammes
+ */
+public class GestionNotes extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/gestionnotes/GestionNotesControllerIhm.java
@@ -0,0 +1,13 @@
+/*
+ * GestionNotes.java                                            16 oct. 2023
+ * IUT de Rodez, info2, pas de droits d'auteur
+ */
+package gestionnotes;
+
+/**
+ * Controlleur de l'application GestionNotes
+ * @author tom.jammes
+ */
+public class GestionNotesControllerIhm {
+    
+}

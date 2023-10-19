e4a0c39f34ebf263f829f0729c999f8cd86863b6
Thomas Lemaire <thomas.lemaire@iut-rodez.fr>
2023-10-19 13:25:33 +0200

    documentation .java

---
+++ src/application/model/GestionNotes.java
@@ -0,0 +1,23 @@
+/* 
+ * GestionNotes.java												19.10.2023
+ * © copyright Chips-Couscous, But Informatique 2, IUT de Rodez (12)
+ */
+
+package application.model;
+
+/**
+ * 
+ */
+public class GestionNotes {
+
+	/**
+	 * Permet à l'utilisateur d'importer les paramètres d'un semestre à 
+	 * partir d'un fichier .csv 
+	 * @param chemin
+	 * @return true
+	 */
+	public boolean importerParametre(String chemin) {
+		// TODO coder la méthode 
+		return true; // Bouchon
+	}
+}

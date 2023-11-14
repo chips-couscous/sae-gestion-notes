4b4cc32c2471e07d01c4170b600afd07be93f2f0
Nguyen Constant <constant.nguyen@iut-rodez.fr>
2023-11-14 10:00:03 +0100

    Changement controlleur

--- src/application/controlleur/Controlleur.java
+++ src/application/controlleur/Controlleur.java
@@ -1,155 +1,172 @@
 package application.controlleur;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
 import application.model.GestionNotes;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 public class Controlleur {
-
 	@FXML
 	BorderPane rootPane; //Récupération de l'ID de la BorderPane principale de la page
-	
 	@FXML
 	TextField note;
-	
+	@FXML
+	Pane paneCentre;
 	@FXML
 	TextField date;
-	
 	@FXML
 	TextArea description;
-	
 	@FXML
-	ComboBox<String> type;
-	
-	@FXML
-	ComboBox<String> ressource;
-	
+	ScrollPane listeNotes;
+
 	@FXML
 	Button valider;
-	
 	@FXML
 	GridPane grilleNotes;
-	
 	@FXML
 	Button validerNom;
-	
 	@FXML
 	Label labelNomEtudiant;
-	
 	@FXML
 	TextField texteNom;
-	
 	@FXML
 	TextField textePrenom;
-	
+
 	@FXML
 	Button boutonImporterFichierProgramme;
-	
 	@FXML
 	Button boutonImporterFichierRessource;
-	
 	@FXML
 	Button boutonPartagerFichier;
-	
+
 	String prenomEtu;
+
 	String nomEtu;
+	
+	int indice = 0;
+
 	FXMLLoader loader = new FXMLLoader();
-	
-	
+
 
 	/**
-	 * Méthode permmettant de lancer d'autres méthodes ou des attributs 
+	 * Méthode permmettant de lancer d'autres méthodes ou des attributs
 	 * directement au lancement de l'application
 	 */
 	public void initialize() {
-		if (note != null && valider != null && date != null && description != null && type != null && ressource != null) {
-			
+		
+		
+		
+	    if (labelNomEtudiant != null && validerNom != null && texteNom != null && textePrenom != null) {
+			validerNom.setOnAction(event -> modifierNom(texteNom, textePrenom));
 
-			valider.setOnAction(event -> validerModifierNote(note,ressource, type, description, date));
 		}
-		
-		if (labelNomEtudiant != null && validerNom != null && texteNom != null && textePrenom != null) {
-	        validerNom.setOnAction(event -> modifierNom(texteNom, textePrenom));
-	        
-	    }
-		
 		if (boutonImporterFichierProgramme != null && boutonImporterFichierRessource != null) {
+			
 			boutonImporterFichierProgramme.setOnAction(event -> selectionnerFichier());
 			boutonImporterFichierRessource.setOnAction(event -> selectionnerFichier());
-		}else if (boutonPartagerFichier != null) {
+		
+		} else if (boutonPartagerFichier != null) {
 			boutonPartagerFichier.setOnAction(event -> selectionnerFichier());
 		}
-		
 		if (labelNomEtudiant != null) {
 			afficherNom();
 		}
-
-	}
-	
-	private void creerNotes2() {       
-        grilleNotes.getChildren().clear();
-        //grilleNotes.add();
-        //GestionNotes.ajouterNote();
-        
-
-    }
-	
-	public void validerModifierNote(TextField note, ComboBox<String> ressource, ComboBox<String> type, TextArea description, TextField date) {
-		Stage stage = (Stage) rootPane.getScene().getWindow();
-        stage.close();
-        creerNote(note.getText(), ressource, type, description.getText(), date.getText());
-      //GestionNotes.ajouterNote();
-	}
-	
-	
-	private GridPane creerNote(String note, ComboBox<String> ressource, ComboBox<String> type, String description, String date) {
-    	GridPane grilleNote = new GridPane();
-    	
-    	grilleNote.setPrefWidth(640);
-    	grilleNote.setPrefHeight(40);
-    	
-    	// Créer des labels pour chaque colonne
-        Label labelNote = new Label(note);
-        Label labelType = new Label(type.getValue());
-        Label labelRessource = new Label(ressource.getValue());  
-
-        // Ajouter les labels à la GridPane
-        grilleNote.add(labelNote, 0, 0); // Colonne 1, Ligne 1
-        grilleNote.add(labelType, 1, 0); // Colonne 2, Ligne 1
-        grilleNote.add(labelRessource, 2, 0); // Colonne 4, Ligne 1
-
-        return grilleNote;
-    }
-      
-	
-	
-
-    public void afficherNom() {
-    	labelNomEtudiant.setText(GestionNotes.afficherIdentite());
-    }
-    
-	public void modifierNom(TextField nom, TextField prenom) {
 		
+		if (note != null && description != null && date != null ) {
+			note.setTextFormatter(patternNote());
+		}
+
+	}
+
+	private void ajouterNote(String note, String description, String date) {
+		
+		
+	    Label labelNote = new Label(note);
+	    Label labelDescription = new Label(description);
+	    Label labelDate = new Label(date);    
+	    
+	    Image imageModifier = new Image(getClass().getResourceAsStream("/application/controlleur/modifier.png"));
+	    Image imageSupprimer = new Image(getClass().getResourceAsStream("/application/controlleur/supprimer.png"));
+
+	    ImageView imageViewModifier = new ImageView(imageModifier);
+	    ImageView imageViewSupprimer = new ImageView(imageSupprimer);
+	    
+
+	    Button modifier = new Button();
+	    modifier.setGraphic(imageViewModifier);
+	    
+	    Button supprimer = new Button();
+	    supprimer.setGraphic(imageViewSupprimer);
+	    
+	    GridPane mainGridPane = (GridPane) ((ScrollPane) ((Pane) ((BorderPane) listeNotes.getScene().getRoot()).getChildren().get(1)).getChildren().get(2)).getContent();
+	    
+	    mainGridPane.add(labelNote, 0, indice);
+	    mainGridPane.add(labelDescription, 1, indice);
+	    mainGridPane.add(labelDate, 2, indice);
+	    mainGridPane.add(modifier, 4, indice);
+	    mainGridPane.add(supprimer, 5, indice);
+	    
+	    GridPane.setHalignment(labelNote, javafx.geometry.HPos.CENTER);
+	    GridPane.setHalignment(labelDescription, javafx.geometry.HPos.CENTER);
+	    GridPane.setHalignment(labelDate, javafx.geometry.HPos.CENTER);
+	    GridPane.setHalignment(modifier, javafx.geometry.HPos.CENTER);
+	    GridPane.setHalignment(supprimer, javafx.geometry.HPos.CENTER);
+	    
+	    labelNote.setStyle("-fx-font-size: 14px; -fx-font-weight: bold; -fx-text-fill: #354B85;");
+		labelDescription.setStyle("-fx-font-size: 14px; -fx-font-weight: bold; -fx-text-fill: #354B85;");
+		labelDate.setStyle("-fx-font-size: 14px; -fx-font-weight: bold; -fx-text-fill: #354B85;");
+		
+		
+		modifier.setMaxSize(30, 30);
+		supprimer.setMaxSize(30, 30);
+		
+		supprimer.setOnMouseEntered(event -> supprimer.setStyle("-fx-background-color: #354B85; -fx-text-fill: #e6e9f0; -fx-background-radius: 100%;"));
+		supprimer.setOnMouseExited(event -> supprimer.setStyle("-fx-background-color: #e6e9f0; -fx-text-fill: #354B85; -fx-background-radius: 100%;"));
+		
+		modifier.setOnMouseEntered(event -> modifier.setStyle("-fx-background-color: #354B85; -fx-text-fill: #e6e9f0; -fx-background-radius: 100%;"));
+		modifier.setOnMouseExited(event -> modifier.setStyle("-fx-background-color: #e6e9f0; -fx-text-fill: #354B85; -fx-background-radius: 100%;"));
+		
+		supprimer.setOnAction(event -> sceneSupprimerNote(mainGridPane, indice));
+		
+		modifier.setStyle("-fx-background-color: #e6e9f0; -fx-text-fill: #354B85; -fx-background-radius: 100%;");
+		supprimer.setStyle("-fx-background-color: #e6e9f0; -fx-text-fill: #354B85; -fx-background-radius: 100%;");
+		indice ++;
+	}
+
+	
+	
+	
+	public void afficherNom() {
+		labelNomEtudiant.setText(GestionNotes.afficherIdentite());
+	}
+
+	public void modifierNom(TextField nom, TextField prenom) {
 		GestionNotes.changerIdentite(nom.getText(), prenom.getText());
 		afficherNom();
-		
 	}
-	
+
 	/**
 	 * Cette méthode permet de récupérer la scene de la page de paramètre d'importation des données
 	 * Elle la charge puis l'affiche en remplacant la scène précedente
@@ -158,17 +175,16 @@ public class Controlleur {
 	@FXML
 	public void changerSceneParametre() {
 		try {
-			
 			loader.setLocation(getClass().getResource("/application/vue/PageParametreImporter.fxml"));
 			Parent nouvelleScene = loader.load();
-			Scene nouvelleSceneObjet = new Scene(nouvelleScene); 
+			Scene nouvelleSceneObjet = new Scene(nouvelleScene);
 			Stage stage = (Stage) rootPane.getScene().getWindow(); // Récupérez la fenêtre actuelle.
 			stage.setScene(nouvelleSceneObjet);
+			System.out.print("Je suis dans Parametre");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 	/**
 	 * Cette méthode permet de récupérer la scene de la page de paramètre de modification d'identité
 	 * Elle la charge puis l'affiche en remplacant la scène précedente
@@ -182,15 +198,11 @@ public class Controlleur {
 			Scene nouvelleSceneObjet = new Scene(nouvelleScene);
 			Stage stage = (Stage) rootPane.getScene().getWindow(); // Récupérez la fenêtre actuelle.
 			stage.setScene(nouvelleSceneObjet);
+			System.out.print("Je suis dans Modifier");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-
-	
-
-
 	/**
 	 * Cette méthode permet de récupérer la scene de la page ou sont présentes les notes
 	 * Elle la charge puis l'affiche en remplacant la scène précedente
@@ -202,16 +214,12 @@ public class Controlleur {
 			loader.setLocation(getClass().getResource("/application/vue/PageNotes.fxml"));
 			Parent nouvelleScene = loader.load();
 			Scene nouvelleSceneObjet = new Scene(nouvelleScene);
-
 			Stage stage = (Stage) rootPane.getScene().getWindow(); // Récupérez la fenêtre actuelle.
-
 			stage.setScene(nouvelleSceneObjet);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-
 	/**
 	 * Cette méthode permet de récupérer la scene de la page de partage des paramètres
 	 * Elle la charge puis l'affiche en remplacant la scène précedente
@@ -223,16 +231,12 @@ public class Controlleur {
 			loader.setLocation(getClass().getResource("/application/vue/PageParametrePartager.fxml"));
 			Parent nouvelleScene = loader.load();
 			Scene nouvelleSceneObjet = new Scene(nouvelleScene);
-
 			Stage stage = (Stage) rootPane.getScene().getWindow(); // Récupérez la fenêtre actuelle.
-
 			stage.setScene(nouvelleSceneObjet);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-
 	/**
 	 * Cette méthode permet de récupérer la scene de la page de paramètre de réinitialisation
 	 * Elle la charge puis l'affiche en remplacant la scène précedente
@@ -250,7 +254,6 @@ public class Controlleur {
 			e.printStackTrace();
 		}
 	}
-
 	/**
 	 * Cette méthode permet de récupérer la scene de la page ou seront présentes les ressrouces
 	 * Elle la charge puis l'affiche en remplacant la scène précedente
@@ -259,123 +262,133 @@ public class Controlleur {
 	@FXML
 	public void changerSceneRessources() {
 		try {
-
 			loader.setLocation(getClass().getResource("/application/vue/PageRessources.fxml"));
 			Parent nouvelleScene = loader.load();
 			Scene nouvelleSceneObjet = new Scene(nouvelleScene);
-
 			Stage stage = (Stage) rootPane.getScene().getWindow(); // Récupérez la fenêtre actuelle.
-			
 			stage.setScene(nouvelleSceneObjet);
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-
+	
+	
 	@FXML
 	public void sceneModifierNote() {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/vue/PageModifierNote.fxml"));
-            Parent root = loader.load();
-            Stage popupStage = new Stage();
-            Stage stage = (Stage) rootPane.getScene().getWindow(); // Récupérez la fenêtre actuelle.
-            popupStage.initOwner(stage); // Utilise la fenêtre principale comme propriétaire
-            popupStage.initModality(Modality.APPLICATION_MODAL);
-            popupStage.setTitle("Popup Window");
-            Scene scene = new Scene(root);
-            popupStage.setScene(scene);
-            popupStage.showAndWait(); // Attend la fermeture de la popup
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    try {
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/vue/PageModifierNote.fxml"));
+	        Parent root = loader.load();
+
+	        Stage popupStage = new Stage();
+
+	        popupStage.initModality(Modality.APPLICATION_MODAL);
+	        popupStage.setTitle("Modifier Note");
+	        Scene popupScene = new Scene(root);
+	        popupStage.setScene(popupScene);
+	        Button boutonValider = (Button) (((Pane) ((GridPane) (popupScene.getRoot().getChildrenUnmodifiable()).get(0)).getChildren().get(7)).getChildren().get(0));
+	        TextField note = (TextField)((Pane) ((GridPane) (popupScene.getRoot().getChildrenUnmodifiable()).get(0)).getChildren().get(1)).getChildren().get(0);
+	        TextArea description = (TextArea)((Pane) ((GridPane) (popupScene.getRoot().getChildrenUnmodifiable()).get(0)).getChildren().get(5)).getChildren().get(0);
+	        TextField date = (TextField) (((Pane) ((GridPane) (popupScene.getRoot().getChildrenUnmodifiable()).get(0)).getChildren().get(6)).getChildren().get(0));
+
+	        boutonValider.setOnAction(e -> {
+	            ajouterNote(note.getText(), description.getText(), date.getText());
+	            popupStage.close();
+	        });
+
+	        popupStage.showAndWait();
+	    } catch(IOException e) {
+	        e.printStackTrace();
+	    }
 	}
 	
+	
+	public void sceneSupprimerNote(GridPane gridPane, int rowIndex) {
+	    try {
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/vue/PageSupprimerNote.fxml"));
+	        Parent root = loader.load();
+
+	        Stage popupStage = new Stage();
+
+	        popupStage.initModality(Modality.APPLICATION_MODAL);
+	        popupStage.setTitle("Supprimer Note");
+	        Scene popupScene = new Scene(root);
+	        popupStage.setScene(popupScene);
+	        Button boutonOui = ((((Button) ((GridPane) (popupScene.getRoot().getChildrenUnmodifiable()).get(0)).getChildren().get(1))));
+	        Button boutonNon = ((((Button) ((GridPane) (popupScene.getRoot().getChildrenUnmodifiable()).get(0)).getChildren().get(2))));
+				        
+	        
+	        boutonOui.setOnMouseEntered(event -> boutonOui.setStyle("-fx-background-color: #354B85; -fx-text-fill: #e6e9f0;"));
+	        boutonOui.setOnMouseExited(event -> boutonOui.setStyle("-fx-background-color: #e6e9f0; -fx-text-fill: #354B85;"));
+	        
+	        boutonNon.setOnMouseEntered(event -> boutonNon.setStyle("-fx-background-color: #354B85; -fx-text-fill: #e6e9f0;"));
+	        boutonNon.setOnMouseExited(event -> boutonNon.setStyle("-fx-background-color: #e6e9f0; -fx-text-fill: #354B85;"));
+
+	        boutonOui.setOnAction(e -> {
+	            supprimerNote(gridPane, rowIndex);
+
+	            popupStage.close();
+	            
+	        });
+	        
+	        boutonNon.setOnAction(e -> {
+	            popupStage.close();
+	        });
+	     
+	        
+
+	        popupStage.showAndWait();
+	    } catch(IOException e) {
+	        e.printStackTrace();
+	    }
+	}
 
 	
 	
-	private TextFormatter<String> createFormatter(Pattern pattern) {
-        UnaryOperator<TextFormatter.Change> filter = change -> {
-            String newText = change.getControlNewText();
-            if (pattern.matcher(newText).matches() || newText.isEmpty()) {
-                return change;
-            } else {
-                return null;
-            }
-        };
+	private TextFormatter<Object> patternNote() {
+		Pattern pattern = Pattern.compile("^1000$|^\\d{1,3}(\\.\\d{0,2})?$");
 
-        return new TextFormatter<>(filter);
-    }
+	    UnaryOperator<TextFormatter.Change> filter = change -> {
+	        String newText = change.getControlNewText();
+	        if (pattern.matcher(newText).matches() || newText.isEmpty()) {
+	            return change;
+	        } else {
+	            return null;
+	        }
+	    };
 
-    private void supprimerNote() {
-        try {
-            Stage stageActuel = (Stage) rootPane.getScene().getWindow();
-            
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/vue/PageSupprimerNote.fxml"));
-            Parent root = loader.load();
+	    return new TextFormatter<>(filter);
+	    
+	}
+	
+	
+	
+	private void supprimerNote(GridPane gridPane, int rowIndex) {
+		
+		ObservableList<Node> children = gridPane.getChildren();
+	    
+	    // Créez une copie de la liste des enfants pour éviter les problèmes de concurrence
+	    List<Node> childrenCopy = new ArrayList<>(children);
 
-            // Créez une nouvelle scène pour la page FXML
-            Scene nouvelleScene = new Scene(root);
+	    // Supprimez tous les nœuds de la ligne spécifiée
+	    childrenCopy.removeIf(node -> GridPane.getRowIndex(node) != null && GridPane.getRowIndex(node) == rowIndex);
 
-            // Créez une nouvelle fenêtre
-            Stage nouveauStage = new Stage();
-            nouveauStage.setTitle("Supprimer une Note");
-
-            // Associez la scène à la nouvelle fenêtre
-            nouveauStage.setScene(nouvelleScene);
-
-            // Montrez la nouvelle fenêtre sans bloquer la fenêtre principale
-            nouveauStage.initOwner(stageActuel);
-            nouveauStage.initModality(Modality.APPLICATION_MODAL);
-            nouveauStage.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
-    
-
-    
-    
-    
-    private GridPane creerNote(TextField note, ComboBox<String> ressource, ComboBox<String> type, TextArea description, TextField date) {
-    	GridPane grilleNote = new GridPane();
-    	
-    	grilleNote.setPrefWidth(640);
-    	grilleNote.setPrefHeight(40);
-    	
-    	// Créer des labels pour chaque colonne
-        Label labelNote = new Label(note.getText());
-        Label labelType = new Label(type.getValue());
-        Label labelRessource = new Label(ressource.getValue());  
-
-        // Ajouter les labels à la GridPane
-        grilleNote.add(labelNote, 0, 0); // Colonne 1, Ligne 1
-        grilleNote.add(labelType, 1, 0); // Colonne 2, Ligne 1
-        grilleNote.add(labelRessource, 2, 0); // Colonne 4, Ligne 1
-
-        return grilleNote;
-    }
-    
-    
-    
-    private void selectionnerFichier() {
-    	FileChooser explorateurFichier = new FileChooser();
-    	explorateurFichier.setTitle("Sélectionner un fichier");
-
-        // Affiche la boîte de dialogue et attend que l'utilisateur sélectionne un fichier
-        Stage stageActuel = (Stage) rootPane.getScene().getWindow();
-        java.io.File fichierChoisi = explorateurFichier.showOpenDialog(stageActuel);
-
-        // Si l'utilisateur a sélectionné un fichier, affiche son chemin
-        if (fichierChoisi != null) {
-            System.out.println("Fichier sélectionné : " + fichierChoisi.getAbsolutePath());
-        } else {
-            System.out.println("Aucun fichier sélectionné.");
-        }
-    }
+	    // Mettez à jour la GridPane avec la nouvelle liste d'enfants
+	    gridPane.getChildren().setAll(childrenCopy);
+	}
+	
+	
+	private void selectionnerFichier() {
+		FileChooser explorateurFichier = new FileChooser();
+		explorateurFichier.setTitle("Sélectionner un fichier");
+		// Affiche la boîte de dialogue et attend que l'utilisateur sélectionne un fichier
+		Stage stageActuel = (Stage) rootPane.getScene().getWindow();
+		java.io.File fichierChoisi = explorateurFichier.showOpenDialog(stageActuel);
+		// Si l'utilisateur a sélectionné un fichier, affiche son chemin
+		if (fichierChoisi != null) {
+			System.out.println("Fichier sélectionné : " + fichierChoisi.getAbsolutePath());
+		} else {
+			System.out.println("Aucun fichier sélectionné.");
+		}
+	}
 }
-

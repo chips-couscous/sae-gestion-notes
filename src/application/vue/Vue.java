07a8bc4109c0ec774b55f15e286b40849735fb38
Lapeyre Tony <tony.lapeyre@iut-rodez.fr>
2023-11-28 08:16:20 +0100

    recup modele + vue

--- src/application/vue/Vue.java
+++ src/application/vue/Vue.java
@@ -32,7 +32,7 @@ public class Vue extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Apllication Note");
+		this.primaryStage.setTitle("Application Note");
 
 		initRootLayout();
 	}
